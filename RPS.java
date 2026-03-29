5e97974d53d2f629baed79e4d6246512a63bc32c
olikurt1 <olikurt1@outlook.com>
2026-03-29 13:51:36 +0100

    Creating functional RPS with rounds

--- RPS.java
+++ RPS.java
@@ -9,6 +9,10 @@ public class RPS{
 
         System.out.print("How many games would you like to play: ");
         int rounds = input.nextInt();
+        //buffer flush 
+        input.nextLine();
+        int userScore = 0;
+        int computerScore = 0;
         for(int round = 0; round < rounds; round++){
             int computerChoiceMap = rnd.nextInt(3);
             String computerChoice = choiceArr[computerChoiceMap];
@@ -22,9 +26,11 @@ public class RPS{
                 }
                 else if(userChoice.equals("Paper")){
                     System.out.println("User Wins");
+                    userScore ++;
                 }
                 else if(userChoice.equals("Scissors")){
                     System.out.println("Computer Wins");
+                    computerScore++;
                 }
             }
             else if(computerChoice.equals("Paper")){
@@ -33,9 +39,11 @@ public class RPS{
                 }
                 else if(userChoice.equals("Scissors")){
                     System.out.println("User Wins");
+                    userScore++;
                 }
                 else if(userChoice.equals("Rock")){
                     System.out.println("Computer Wins");
+                    computerScore++;
                 }
             }
             else if(computerChoice.equals("Scissors")){
@@ -44,14 +52,25 @@ public class RPS{
                 }
                 else if(userChoice.equals("Rock")){
                     System.out.println("User Wins");
+                    userScore++;
                 }
                 else if(userChoice.equals("Paper")){
                     System.out.println("Computer Wins");
+                    computerScore++;
                 }
             }
             else{
                 System.out.println("Incorrect input or mismatch");
             }
+        }
+        if(userScore > computerScore){
+            System.out.println("User Wins");
+        }
+        else if(computerScore > userScore){
+            System.out.println("Computer Wins");
+        }
+        else if(computerScore == userScore){
+            System.out.println("Draw");
         }
         input.close();   
     }
